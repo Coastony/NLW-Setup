4b1bbd24c932ae16e373cbe7c2339a25bc814fbb
Sérgio Bueno <84647000+Coastony@users.noreply.github.com>
2023-01-20 22:52:41 -0300

    Add files via upload

---
+++ web/src/components/Habit.tsx
@@ -0,0 +1,13 @@
+import "./habit.css";
+
+interface HabitProps {
+  completed: number;
+}
+
+export function Habit(props: HabitProps) {
+  return (
+    <p className="bg-zinc-900 w-10 h-10 text-white rounded m-2 flex items-center justify-center">
+      {props.completed}
+    </p>
+  );
+}
